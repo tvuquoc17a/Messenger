7c5f90010fd8459b3d90e2f73c609be482a3a038
ouch_ouch <2055010244@kientruchanoi.edu.vn>
2024-03-21 11:30:09 +0700

    before migrate to AndroidX

---
+++ app/src/main/java/com/example/messenger/SplashActivity.kt
@@ -0,0 +1,41 @@
+package com.example.messenger
+
+import android.app.ActivityOptions
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.util.Pair
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var topAnimation: Animation
+    private lateinit var logoImage: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        logoImage = findViewById(R.id.imgLogo)
+
+        topAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+
+            override fun onAnimationEnd(animation: Animation?) {
+                val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+                val pairs = arrayOf<Pair<View, String>>(Pair(logoImage, "logo"))
+                val options = ActivityOptions.makeSceneTransitionAnimation(this@SplashActivity, *pairs)
+                startActivity(intent, options.toBundle())
+                finish()
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+        })
+
+        logoImage.startAnimation(topAnimation)
+    }
+}

e1b33ae1348a7171ec4df7ff8d091abb962fec72
ouch_ouch <2055010244@kientruchanoi.edu.vn>
2024-04-18 21:36:11 +0700

    before test signalR

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
     implementation("com.google.firebase:firebase-database:20.3.1")
     implementation("com.google.firebase:firebase-storage:20.3.0")
     implementation("androidx.activity:activity:1.8.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -65,5 +67,8 @@ dependencies {
 
     //circle image view
     implementation ("de.hdodenhof:circleimageview:3.1.0")
+
+    implementation( "android.arch.navigation:navigation-fragment-ktx:1.0.0-alpha07")
+    implementation( "android.arch.navigation:navigation-ui-ktx:1.0.0-alpha07")
 
 }
--- app/src/main/java/com/example/messenger/ChatsActivity.kt
+++ app/src/main/java/com/example/messenger/ChatsActivity.kt
@@ -5,16 +5,62 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.messenger.databinding.ActivityChatsBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ChatsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityChatsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityChatsBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_chats)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = Navigation.findNavController(this, R.id.fragments)
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
+        bottomNavigationView.selectedItemId = R.id.chatsFragment
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.chatsFragment -> {
+                    binding.toolBarChats.title = "Chats"
+                    true
+                }
+
+                R.id.callFragment -> {
+                    binding.toolBarChats.title = "Calls"
+                    true
+                }
+
+                R.id.peopleFragment -> {
+                    binding.toolBarChats.title = "People"
+                    true
+                }
+
+                R.id.storyFragment -> {
+                    binding.toolBarChats.title = "Stories"
+                    true
+                }
+
+                else -> {
+                    false
+                }
+            }
+        }
+
+
     }
+
 }
